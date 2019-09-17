da1d910e9c65a6e2f555b5fc463f80dd465d7aaa
Simone Boscolo Berto <simone.boscoloberto2@ibm.com>
2019-09-17 13:07:42 +0100

    Fix mini calendar and double click on both timeline and calendar

--- src/TimeLineBar/TimeLineBar.js
+++ src/TimeLineBar/TimeLineBar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import { subDays, addDays, startOfToday, format } from 'date-fns';
+import { subDays, addDays, startOfToday, format, isEqual } from 'date-fns';
 import { Timeline, DatesPicker } from './components';
 
 const useStyles = makeStyles({
@@ -28,6 +28,13 @@ const formatDate = date => {
   return format(date, 'yyyy-MM-dd');
 };
 
+const areDatesNotEqual = (newDates, currentDates) => {
+    const [newStartDate, newEndDate] = newDates;
+    const [currentStartDate, currentEndDate] = currentDates;
+    return !isEqual(newStartDate,currentStartDate) || !isEqual(newEndDate,currentEndDate)
+  }
+
+
 const TimeLineBar = props => {
   const classes = useStyles();
   const { onChangeDates } = props;
@@ -42,21 +49,25 @@ const TimeLineBar = props => {
 
   const handleDatesChanged = dates => {
     console.log('Timeline changed', dates);
-    setSelectedDates(dates);
-    onChangeDates(dates);
+    if( areDatesNotEqual(dates, selectedDates)) {
+        setSelectedDates(dates);
+        onChangeDates(dates);
+    }
   };
 
   const handleCalendarChange = dates => {
     console.log('Calendar changed', dates);
     const [calStartDate, calEndDate] = dates;
-    if (calEndDate >= endDate) {
-      setEndDate(addDays(calEndDate, 30));
+    if (areDatesNotEqual(dates, selectedDates)) {
+        if (calEndDate >= endDate) {
+          setEndDate(addDays(calEndDate, 30));
+        }
+        if (calStartDate <= startDate) {
+          setStartDate(subDays(calStartDate, 30));
+        }
+        setSelectedDates(dates);
+        onChangeDates(dates);
     }
-    if (calStartDate <= startDate) {
-      setStartDate(subDays(calStartDate, 30));
-    }
-    setSelectedDates(dates);
-    onChangeDates(dates);
   };
 
   return (
